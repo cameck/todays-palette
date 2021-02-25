9ffdf51bb30883227fedfadd3f490133786d622b
Cameron Eckelberry <cameck@gmail.com>
2021-02-24 16:59:14 -0800

    add more palettes and write to dom

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import palettes from 'nice-color-palettes';
+import palettes from 'nice-color-palettes/500';
 import './style.css';
 
 const makeDiv = (color, body) => {
-  const newdiv = document.createElement('div');
-  newdiv.style.background = color;
-  body.appendChild(newdiv);
+  const newDiv = document.createElement('div');
+  const newP = document.createElement('p');
+  newP.innerHTML = color;
+  newDiv.appendChild(newP);
+  newDiv.style.background = color;
+  body.appendChild(newDiv);
 };
 
 const init = () => {
   const body = document.getElementsByTagName('body')[0];
   const palette = palettes[Math.floor(Math.random() * palettes.length)];
-  palette.forEach((color) => makeDiv(color, body));
+  palette.map((color) => makeDiv(color, body));
 };
 
 init();
