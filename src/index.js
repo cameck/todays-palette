39d8b7ae5a19577f9ce73698d4f9d0095f3b26c2
Cameron Eckelberry <cameck@gmail.com>
2021-02-24 20:45:00 -0800

    import the assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import palettes from 'nice-color-palettes/500';
 import tinyColor from 'tinycolor2';
 
+import './assets/android-chrome-192x192.png';
+import './assets/android-chrome-512x512.png';
+import './assets/apple-touch-icon.png';
+import './assets/favicon-16x16.png';
+import './assets/favicon-32x32.png';
+import './assets/favicon.ico';
+import './assets/site.webmanifest';
 import './style.css';
 
 const makeDiv = (color, body) => {
