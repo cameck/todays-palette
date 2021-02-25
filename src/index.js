c726a35f154043764d8acc1ca7ca61f7eb18d440
Cameron Eckelberry <cameck@gmail.com>
2021-02-24 16:32:55 -0800

    basic palette maker design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-const palettes = require('nice-color-palettes');
+import palettes from 'nice-color-palettes';
+import './style.css';
+
+const makeDiv = (color, body) => {
+  const newdiv = document.createElement('div');
+  newdiv.style.background = color;
+  body.appendChild(newdiv);
+};
+
+const init = () => {
+  const body = document.getElementsByTagName('body')[0];
+  const palette = palettes[Math.floor(Math.random() * palettes.length)];
+  palette.forEach((color) => makeDiv(color, body));
+};
+
+init();
