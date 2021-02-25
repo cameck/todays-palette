8eec97cef11fa9d7ee02d13616b9b81e46efede5
Cameron Eckelberry <cameck@gmail.com>
2021-02-25 10:41:39 -0800

    prettier

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-import palettes from 'nice-color-palettes';
-import tinyColor from 'tinycolor2';
+import palettes from "nice-color-palettes";
+import tinyColor from "tinycolor2";
 
-import lisaPalettes from './data/colorLisa.json';
+import lisaPalettes from "./data/colorLisa.json";
 
-import './assets/android-chrome-192x192.png';
-import './assets/android-chrome-512x512.png';
-import './assets/apple-touch-icon.png';
-import './assets/favicon-16x16.png';
-import './assets/favicon-32x32.png';
-import './assets/favicon.ico';
-import './assets/site.webmanifest';
-import './style.css';
+import "./assets/android-chrome-192x192.png";
+import "./assets/android-chrome-512x512.png";
+import "./assets/apple-touch-icon.png";
+import "./assets/favicon-16x16.png";
+import "./assets/favicon-32x32.png";
+import "./assets/favicon.ico";
+import "./assets/site.webmanifest";
+import "./style.css";
 
-const lisaArr = Object.keys(lisaPalettes).map((palette) => lisaPalettes[palette]);
+const lisaArr = Object.keys(lisaPalettes).map(
+  (palette) => lisaPalettes[palette]
+);
 const allPalettes = palettes.concat(lisaArr);
 
 const makeDiv = (color, body) => {
-  const newDiv = document.createElement('div');
-  const newP = document.createElement('p');
+  const newDiv = document.createElement("div");
+  const newP = document.createElement("p");
   newP.innerHTML = color;
   const isLight = tinyColor(color).isLight();
   if (isLight) {
-    newP.style.color = '#0f0f0f';
+    newP.style.color = "#0f0f0f";
   }
 
   newDiv.appendChild(newP);
@@ -30,7 +32,7 @@ const makeDiv = (color, body) => {
 };
 
 const init = () => {
-  const body = document.getElementsByTagName('body')[0];
+  const body = document.getElementsByTagName("body")[0];
   const palette = allPalettes[Math.floor(Math.random() * allPalettes.length)];
   palette.map((color) => makeDiv(color, body));
 };
